474d5201a18aad98829527fd76b9a33c178fde9e
Natanael1234 <{ID}+{username}@users.noreply.github.com>
2024-09-30 15:21:24 -0300

    route config test

--- src/app/routes.spec.ts
+++ src/app/routes.spec.ts
@@ -5,31 +5,29 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NewPasswordComponent } from './pages/new-password/new-password.component';
 
-const expectedRoutes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home Page' },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    title: 'Register Page',
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-    title: 'Login Page',
-  },
-  {
-    path: 'new-password/:hash',
-    component: NewPasswordComponent,
-    title: 'New Password Page',
-  },
-];
-
 describe('routeConfig', () => {
   it('should be defined', () => {
     expect(routeConfig).toBeDefined();
   });
 
   it('should have valid keys and values', () => {
-    expect(routeConfig).toEqual(routeConfig);
+    expect(routeConfig).toEqual([
+      { path: '', component: HomeComponent, title: 'Home Page' },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register Page',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login Page',
+      },
+      {
+        path: 'new-password/:hash',
+        component: NewPasswordComponent,
+        title: 'New Password Page',
+      },
+    ]);
   });
 });
