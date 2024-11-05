9687be0d23502a912da4f29c23f45587ce5c5ca5
Natanael1234 <{ID}+{username}@users.noreply.github.com>
2024-11-05 18:10:27 -0300

    comment

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class HttpService {
-  apiUrl = 'http://localhost:3000/api'; // TODO:
+  apiUrl = 'http://localhost:3000/api'; // TODO: move api url to config file
 
   http: HttpClient = inject(HttpClient);
 
