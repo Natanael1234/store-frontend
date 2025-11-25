ef5f7869b41f784582986d408f618c175c0c4870
Natanael1234 <{ID}+{username}@users.noreply.github.com>
2025-11-25 20:44:01 -0300

    fixes toolbar test error

--- src/app/components/toolbar/toolbar.component.spec.ts
+++ src/app/components/toolbar/toolbar.component.spec.ts
@@ -1,23 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
+    let component: ToolbarComponent;
+    let fixture: ComponentFixture<ToolbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ToolbarComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ToolbarComponent],
+            providers: [provideRouter([])],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ToolbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -7,12 +7,60 @@ import { leftMouseClickFilter } from '../../utils/mouse-events/mouse-click-filte
 
 @Component({
     selector: 'app-toolbar',
-    imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
-    templateUrl: './toolbar.component.html',
-    styleUrl: './toolbar.component.scss',
+    imports: [
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        RouterModule,
+        acrout,
+    ],
+    styles: `
+        .spacer {
+            flex: 1 1 auto;
+        }
+
+        .mat-toolbar {
+            gap: 0.5em;
+        }
+    `,
+    template: `
+        <mat-toolbar>
+            <!-- theme button -->
+            <button
+                mat-icon-button
+                aria-label="Botão menu"
+                color="primary"
+                (click)="fireToggleMenuEvent($event)">
+                <mat-icon>menu</mat-icon>
+            </button>
+
+            <!-- title -->
+            <span>Meus Produtos</span>
+
+            <!-- spacer -->
+            <span class="spacer"></span>
+
+            <!-- theme button -->
+            <button
+                mat-icon-button
+                aria-label="Botão alternar tema"
+                (click)="fireToggleThemeEvent($event)">
+                <mat-icon>{{ theme }}</mat-icon>
+            </button>
+
+            <!-- user button -->
+
+            <button
+                mat-icon-button
+                aria-label="Botão usuário"
+                [routerLink]="'login'">
+                <mat-icon>person</mat-icon>
+            </button>
+        </mat-toolbar>
+    `,
 })
 export class ToolbarComponent {
-    @Input() theme: 'light_mode' | 'dark_mode' = 'light_mode';
+    @Input() theme: 'light_mode' | 'dark_mode' = 'light_mode'; // TODO: move to enum
     @Output() public toggleMenu = new EventEmitter();
     @Output() public toggleTheme = new EventEmitter();
 
